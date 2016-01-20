b374c0dce6f26fa551bdb184bc709e9b92cd32a3
Barry Scott <barry@barrys-emacs.org>
2016-01-20 09:34:02 +0000

    script to help with installing package for testing localy.

---
+++ localinstall.cmd
@@ -0,0 +1,15 @@
+setlocal
+if "%1" == "32" (
+    set PY_VER=3.5-32
+) else if "%1" == "64" (
+    set PY_VER=3.5
+) else (
+    echo Error: %%1 must be 32 or 64
+    goto :eof
+)
+
+for %%i in (dist\*.whl) do set WHEEL=%%~fi
+cd %userprofile%
+py -%PY_VER% -m pip install -U %WHEEL%
+
+endlocal
