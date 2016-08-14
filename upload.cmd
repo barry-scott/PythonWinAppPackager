bea4c3113c3f9f7ad21ad34a3714aeeedb4d7fd8
Barry Scott <barry@barrys-emacs.org>
2016-08-14 10:24:11 +0100

    check for missing %2

--- upload.cmd
+++ upload.cmd
@@ -5,6 +5,11 @@ if "%1" == "32" (
     set PY_VER=3.5
 )
 
+if "%2" == "" (
+    echo provide password as quoted %%2 arg
+    goto :eof
+)
 
-py -%PY_VER% -m twine upload dist\*
+set HOME=%USERPROFILE%
+py -%PY_VER% -m twine upload -p %2 dist\*
 endlocal
